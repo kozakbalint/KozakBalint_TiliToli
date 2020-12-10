15016bbbd353c17fb914860b3a35c3f37bd1e61a
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2020-12-10 19:22:04 +0100

    Board Size Input Handling

--- TiliToli/MainWindow.xaml.cs
+++ TiliToli/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace TiliToli
     /// </summary>
     public partial class MainWindow : Window
     {
+        int boardSizeValue = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Game_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                boardSizeValue = Int32.Parse(boardSizeInput.Text);
+                if (boardSizeValue < 3)
+                {
+                    MessageBox.Show("Nagyobb számot adj meg.");
+                }
+                else if (boardSizeValue > 10)
+                {
+                    MessageBox.Show("Kisebb számot adj meg.");
+                }
+                else
+                {
+                    //TODO: Game method.
+                }
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Számot adjon meg a tábla méretének.");
+            }
         }
     }
 }
